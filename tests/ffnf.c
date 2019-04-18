15c9a750955a8e2f637ccb0a7022bbd649cba5e5
tochy <tochytube@gmail.com>
2019-04-18 08:36:23 -0500

    Added Files

---
+++ tests/ffnf.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  char * ptr1 = ( char * ) malloc ( 1000 );
+  char * ptr2 = ( char * ) malloc ( 6000 );
+  char * ptr3 = ( char * ) malloc ( 1000 );
+
+  printf("ptr1 - %p\n",ptr1);
+    printf("ptr2 - %p\n",ptr2);
+      printf("ptr3 - %p\n",ptr3);
+
+  printf("First fit should pick this one: %p\n", ptr1 );
+  printf("Next fit should pick this one: %p\n", ptr3 );
+
+  free( ptr1 );
+  free( ptr2 );
+  free( ptr3 );
+
+  char * ptr5 = ( char * ) malloc ( 6000 );
+
+  ptr5 = ptr5;
+
+  char * ptr4 = ( char * ) malloc ( 1000 );
+  printf("Chosen address: %p\n", ptr4 );
+
+  return 0;
+}
